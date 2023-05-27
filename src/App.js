642c4704019c768b06b0a3d9e77bcc1b6248658d
CRKarthick <karthick25071998@gmail.com>
2023-05-28 03:17:50 +0530

    Theaters with movies and shows added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Alert from './components/Alert';
+import Theaters from './components/Theaters';
+import NotFound from './components/NotFound';
 
 function App() {
     const [mailId, setMailId] = useState('');
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch(
+                `https://zincubate.in/api/MovieTicketChecker?action=getAllDetails&user_mail_id=${mailId}`,
+                {
+                    method: 'POST',
+                }
+            );
+            const json = await response.json();
+            setData(json);
+        }
+        if (mailId) fetchData();
+        // fetch(
+        //     `https://zincubate.in/api/MovieTicketChecker?action=getAllDetails&user_mail_id=${mailId}`,
+        //     {
+        //         method: 'POST',
+        //     }
+        // )
+        //     .then(response => response.json())
+        //     .then(json => setData(json));
+    }, [mailId]);
+
     return (
         <div className='App'>
             <Navbar setAppMailId={setMailId} />
             {!mailId && <Alert />}
+            {/* {Object.keys(data).length > 0 &&
+                data.theatre.map(th => {
+                    return <div key={th.theatre_name}>{th.theatre_name}</div>;
+                })} */}
+            {Object.keys(data).length > 0 && (
+                <Routes>
+                    <Route path='/' element={<Theaters data={data} />} />
+                    <Route path='/*' element={<NotFound />} />
+                </Routes>
+            )}
         </div>
     );
 }
---
+++ src/components/Theaters.js
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from 'react';
+
+const Theaters = ({ data }) => {
+    console.log(data);
+    return (
+        <div className='container my-5'>
+            <div className='card'>
+                <div className='card-header'>
+                    <h2>Theaters</h2>
+                </div>
+                {data.theatre.map(theatre => (
+                    <div className='card-body' key={theatre.theatre_name}>
+                        <div className='card mb-3'>
+                            <div className='row g-0'>
+                                <div className='col-md-4'>
+                                    <img
+                                        src={theatre.thumbnail_url}
+                                        className='img-fluid rounded-start'
+                                        alt='theatre_img'
+                                        style={{ width: '100%' }}
+                                    />
+                                </div>
+                                <div className='col-md-8'>
+                                    <div className='card-body'>
+                                        <h5 className='card-title'>
+                                            <div className='row'>
+                                                <div className='col-md-10'>
+                                                    <a href={theatre.website}>
+                                                        {theatre.theatre_name}
+                                                    </a>
+                                                </div>
+                                                <div className='col-md-2 float-right text-end'>
+                                                    ⭐ {theatre.customer_rating}
+                                                </div>
+                                            </div>
+                                        </h5>
+                                        <p class='card-text'>
+                                            <small class='text-muted'>
+                                                {theatre.address}
+                                            </small>
+                                        </p>
+                                        <hr />
+                                        <div className='row'>
+                                            <div className='col-3'>
+                                                <div className='card border-success mb-3'>
+                                                    <div
+                                                        className='card-header text-truncate'
+                                                        title={
+                                                            theatre.show1_movie
+                                                        }
+                                                    >
+                                                        {theatre.show1_movie}
+                                                    </div>
+                                                    <div className='card-body text-success text-center'>
+                                                        <h5 className='card-title'>
+                                                            {theatre.show1_time}
+                                                        </h5>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className='col-3'>
+                                                <div className='card border-success mb-3'>
+                                                    <div
+                                                        className='card-header text-truncate'
+                                                        title={
+                                                            theatre.show2_movie
+                                                        }
+                                                    >
+                                                        {theatre.show2_movie}
+                                                    </div>
+                                                    <div className='card-body text-success text-center'>
+                                                        <h5 className='card-title'>
+                                                            {theatre.show2_time}
+                                                        </h5>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className='col-3'>
+                                                <div className='card border-success mb-3'>
+                                                    <div
+                                                        className='card-header text-truncate'
+                                                        title={
+                                                            theatre.show3_movie
+                                                        }
+                                                    >
+                                                        {theatre.show3_movie}
+                                                    </div>
+                                                    <div className='card-body text-success text-center'>
+                                                        <h5 className='card-title'>
+                                                            {theatre.show3_time}
+                                                        </h5>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className='col-3'>
+                                                <div className='card border-success mb-3'>
+                                                    <div
+                                                        className='card-header text-truncate'
+                                                        title={
+                                                            theatre.show4_movie
+                                                        }
+                                                    >
+                                                        {theatre.show4_movie}
+                                                    </div>
+                                                    <div className='card-body text-success text-center  '>
+                                                        <h5 className='card-title'>
+                                                            {theatre.show4_time}
+                                                        </h5>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Theaters;
